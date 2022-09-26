50b85c4f92ab4d8693472ea47096bb21936eb767
dzJP <113700179+dzJP@users.noreply.github.com>
2022-09-26 15:59:31 +0200

    Update Labb1.java

--- Labb1.java
+++ Labb1.java
@@ -1,5 +1,7 @@
 package Section5ControlFlowStatements.src;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.util.*;
 
 public class Labb1 {
@@ -9,42 +11,50 @@ public class Labb1 {
         Scanner sc = new Scanner(System.in);
         Scanner end = new Scanner(System.in);
 
+
         boolean run = true;
+
         while (run) {
             System.out.println("""
                     1. Upp och ner. 
-                    2. Min Max
-                    3. Sten Sax Påse
-                    4. Ordning och reda
+                    2. Min Max.
+                    3. Sten Sax Påse.
+                    4. Ordning och reda.
                     e. Avsluta. """);
 
             int yourChoice = 0;
 
+
             if (input.hasNextInt()) {
+
                 yourChoice = input.nextInt();
-
-
                 if (yourChoice == 1) {
+                    Scanner userExit = new Scanner(System.in);
 
 
-                    System.out.println("Ditt val är Upp och ner. \nHur många namn vill du lägga till? ");
-                    int n = sc.nextInt();
-                    System.out.println("Lägg till " + n + " namn. \nTryck på Enter mellan varje inmatning eller" +
-                            " skriv end för att avbryta programmet.");
-                    sc.nextLine();
-
-                    String[] names = new String[n]; // datatyp array av namnet namn som sparar input i en variabel
-                    // store names in an array
-
-                    for (int i = names.length - 1; i >= 0; i--) {
-                        names[i] = sc.nextLine();
+                    System.out.println("Val 1: Upp och ner. \nLägg till namn och tryck på Enter efter varje " +
+                            "inmatning. ");
+                    String[] names = new String[3];
+                    for (int i = 0; i < names.length; i++) {
+                        names[i] = input.next();
                     }
-                    System.out.println(Arrays.toString(names));
 
+
+                    System.out.println("Tryck på Enter för att fortsätta eller skriv end för att avsluta.");
+
+                    String userInput = end.nextLine();
+                    if (userInput.equalsIgnoreCase("end")) {
+                        System.out.println("Avslutar programmet..");
+                        System.exit(0);
+                    }
+                    Arrays.sort(names);
+                    System.out.println(names[2] + " " + names[1] + " " + names[0]);
                 }
 
+
+
                 if (yourChoice == 2) {
-                    System.out.println("Ditt val är Min Max.");
+                    System.out.println("Val 2: Min Max.");
 
 
                     double max = 0;
@@ -80,25 +90,27 @@ public class Labb1 {
 
                 if (yourChoice == 3) {
 
-                    System.out.println("Ditt val är: Sten Sax Påse.");
+                    System.out.println("\nVal 3: Sten, Sax, Påse.");
                     Scanner in = new Scanner(System.in);
+                    int playerScore = 0;
+                    int botScore = 0;
 
                     //Use a while(true) loop and only break the loop if the user wants to quit
                     while (true) {
 
                         //Get the user's move through user input
-                        System.out.print("Skriv sten, sax, påse eller skriv quit för att avsluta programmet.");
+                        System.out.println("\nSkriv sten, sax, påse eller skriv end för att avsluta programmet.");
                         String myInput = in.nextLine();
 
                         //Check if the user wants to quit the game
-                        if (myInput.equalsIgnoreCase("quit")) {
+                        if (myInput.equalsIgnoreCase("end")) {
                             break;
                         }
 
                         //Check if the user's move is valid (rock, paper, or scissors = false)
-                        if (!myInput.equals("sten") && !myInput.equals("paper") && !myInput.equals("sax")) {
+                        if (!myInput.equals("sten") && !myInput.equals("påse") && !myInput.equals("sax")) {
 
-                            System.out.println("Your move isn't valid!");
+                            System.out.println("Felaktigt drag!");
 
                         } else {
 
@@ -114,72 +126,86 @@ public class Labb1 {
                             } else {
                                 botChoice = "sax";
                             }
-                            System.out.println("Bottens drag: " + botChoice);
+                            System.out.println("\nDatorns drag: " + botChoice);
 
                             //Print the results of the game: tie, lose, win
                             if (myInput.equalsIgnoreCase(botChoice)) {
                                 System.out.println("Oavgjort.");
+                                System.out.println("Du har: " + playerScore + " poäng. " + "\nDatorn har: " + botScore + " poäng.");
+
                             } else if ((myInput.equals("sten") && botChoice.equals("sax")) ||
                                     (myInput.equals("sax") && botChoice.equals("påse")) ||
                                     (myInput.equals("påse") && botChoice.equals("sten"))) {
                                 System.out.println("Du vann!");
+                                playerScore = playerScore + 1;
+                                System.out.println("Du har: " + playerScore + " poäng." + "\nDatorn har: " + botScore + " poäng.");
+
                             } else {
-                                System.out.println("Du förlorade!");
+                                System.out.println("Du förlorade omgången!");
+                                botScore = botScore + 1;
+                                System.out.println("Du har: " + playerScore + " poäng." + "\nDatorn har: " + botScore + " poäng.");
+
+                            }
+                            if (playerScore == 3) {
+                                System.out.println("\n******************" + "\nGrattis! Du vann!" + "\n******************\n");
+                                break;
+                            }
+                            if (botScore == 3) {
+                                System.out.println("\n******************" + "\nDu förlorade! =(" + "\n******************\n");
+                                break;
                             }
                         }
                     }
                 }
 
-
                 if (yourChoice == 4) {
-                        int[] numbers;
-                        int sum = 0;
-                        int max = 0;
-                        int min = Integer.MAX_VALUE;
-                        numbers = new int[5];
 
-                        System.out.println("Lägg till tal.");
+                    int[] numbers;
+                    int sum = 0;
+                    int max = 0;
+                    int min = Integer.MAX_VALUE;
+                    numbers = new int[5];
 
-                        for (int i = 0; i < numbers.length; i++) {
-                            numbers[i] = input.nextInt();
-                            sum = sum + numbers[i];
+                    System.out.println("Lägg till tal.");
+
+                    for (int i = 0; i < numbers.length; i++) {
+                        numbers[i] = input.nextInt();
+                        sum = sum + numbers[i];
+                    }
+
+                    for (int i = 0; i < 5; i++) {
+                        int number = numbers[i];
+                        //Add the number to the total
+
+
+                        //If this is the highest number we've encountered, set as the max.
+                        if (max < number) {
+                            max = number;
                         }
-
-                        for (int i = 0; i < 5; i++) {
-
-                            int number = numbers[i];
-                            //Add the number to the total
-
-
-                            //If this is the highest number we've encountered, set as the max.
-                            if (max < number) {
-                                max = number;
-                            }
-                            //If this is the lowest number we've encountered, set as min.
-                            if (min > number) {
-                                min = number;
-                            }
+                        //If this is the lowest number we've encountered, set as min.
+                        if (min > number) {
+                            min = number;
                         }
+                    }
 
-                        System.out.print(Arrays.toString(numbers).replaceAll("[\\[|,\\]]", ""));
-                        System.out.print("\nStörst:" + max);
-                        System.out.print("\nMinst:" + min);
-                        System.out.print("\nNäst minst:" + numbers[3]);
-                        System.out.print("\nNäst störst:" + numbers[1]);
+                    System.out.print(Arrays.toString(numbers).replaceAll("[\\[|,\\]]", ""));
+                    System.out.print("\nStörst:" + max);
+                    System.out.print("\nMinst:" + min);
+                    System.out.print("\nNäst minst:" + numbers[3]);
+                    System.out.print("\nNäst störst:" + numbers[1]);
 
-                        System.out.print("\nSumma "+sum);
-                        Arrays.sort(numbers);
-                        if (numbers == numbers) {
-                            System.out.print("\ntrue"); // true
-                        } else {
-                            System.out.print("\nfalse"); // false
-                        }
+                    System.out.print("\nSumma " + sum);
+                    Arrays.sort(numbers);
+                    if (numbers == numbers) {
+                        System.out.print("\ntrue"); // true
+                    } else {
+                        System.out.print("\nfalse"); // false
+                    }
 
-                        System.out.println("\nTryck på Enter för att börja om.");
-                        sc.nextLine();
+                    System.out.println("\nTryck på Enter för att börja om.");
+                    sc.nextLine();
                 }
             }
         }
     }
 }
-
